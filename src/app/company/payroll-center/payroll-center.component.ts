237e7dd5423a0c3c2640e026a0603e6ef1ec63ae
Hui Kou <hui.kou@ApexHCM.com>
2018-09-19 17:03:35 -0400

    committ

--- src/app/company/payroll-center/payroll-center.component.ts
+++ src/app/company/payroll-center/payroll-center.component.ts
@@ -4,8 +4,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Response } from "@angular/http";
 import { Observable } from 'rxjs';
-
-
+import "ag-grid-enterprise";
+declare var $: any;
 @Component({
   selector: 'app-payroll-center',
   templateUrl: './payroll-center.component.html',
@@ -23,6 +23,15 @@ export class PayrollCenterComponent implements OnInit {
   private rowData1;
   private columnDefs1;
   private defaultColDef1;
+
+  private rowData2;
+  private columnDefs2;
+  private defaultColDef2;
+  private gridApi1;
+  private detailCellRendererParams;
+  private components;
+  
+
   topOptions = {alignedGrids: []};
   bottomOptions = {alignedGrids: []};
 
@@ -48,200 +57,175 @@ export class PayrollCenterComponent implements OnInit {
     });
   
 
-   
-    //this.columnDefs.aggFunc = 'sum';
+    this.components = { datePicker: getDatePicker() };
 
-    // ];
-    this.topOptions.alignedGrids.push(this.bottomOptions);
-    this.bottomOptions.alignedGrids.push(this.topOptions);
+     this.topOptions.alignedGrids.push(this.bottomOptions);
+     this.bottomOptions.alignedGrids.push(this.topOptions);
 
-
-    this.rowData1 = [
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "prs",
-        exteriorColour: "cb",
-        interiorColour: "fg",
-        price: 72000
-      },
-      {
-        make: "tyt",
-        exteriorColour: "fg",
-        interiorColour: "bw",
-        price: 35000
-      },
-      {
-        make: "frd",
-        exteriorColour: "bw",
-        interiorColour: "cb",
-        price: 32000
-      }
-    ];
-    this.columnDefs1 = [
-      {
-        headerName: "Make",
-        field: "make",
-        cellEditor: "select",
-        cellEditorParams: { values: extractValues1(carMappings) },
-        valueFormatter: function(params) {
-          return lookupValue1(carMappings, params.value);
-        },
-        valueParser: function(params) {
-          return lookupKey1(carMappings, params.newValue);
+    this.detailCellRendererParams = {
+      detailGridOptions: {
+        columnDefs: [
+          { field: "payrollRecordID" ,
+          },
+          { field: "taxName" },
+          { field: "taxPaymentNumber" },
+          {
+            field: "taxDueDate",
+            valueFormatter: "x.toLocaleString() + 's'"
+          },
+          { field: "taxRateCompany" }
+        ],
+        onFirstDataRendered(params) {
+          params.api.sizeColumnsToFit();
         }
       },
-      {
-        headerName: "Exterior Colour",
-        field: "exteriorColour",
-        cellEditor: "agRichSelectCellEditor",
-        cellEditorParams: {
-          values: extractValues1(colourMappings),
-          cellRenderer: colorCellRenderer
-        },
-        filter: "agSetColumnFilter",
-        filterParams: {
-          values: extractValues1(colourMappings),
-          cellRenderer: colorCellRenderer
-        },
-        valueFormatter: function(params) {
-          return lookupValue1(colourMappings, params.value);
-        },
-        valueParser: function(params) {
-          return lookupKey1(colourMappings, params.newValue);
-        },
-        cellRenderer: colorCellRenderer
-      },
-      {
-        headerName: "Interior Colour",
-        field: "interiorColour",
-        cellEditor: "agTextCellEditor",
-        cellEditorParams: { useFormatter: true },
-        filter: "agSetColumnFilter",
-        filterParams: {
-          values: extractValues1(colourMappings),
-          cellRenderer: colorCellRenderer
-        },
-        valueFormatter: function(params) {
-          return lookupValue1(colourMappings, params.value);
-        },
-        valueParser: function(params) {
-          return lookupKey1(colourMappings, params.newValue);
-        },
-        cellRenderer: colorCellRenderer
-      },
-      {
-        headerName: "Retail Price",
-        field: "price",
-        colId: "retailPrice",
-
-        valueGetter: function(params) {
-          return params.data.price;
-        },
-        valueFormatter: currencyFormatter,
-        valueSetter: numberValueSetter
-      },
-      {
-        headerName: "Retail Price (incl Taxes)",
-        editable: false,
-        valueGetter: function(params) {
-          return params.getValue("retailPrice") * 1.2;
-        },
-        valueFormatter: currencyFormatter
+      getDetailRowData: function(params) {
+        params.successCallback(params.data.payrollTaxes);
       }
-    ];
-    this.defaultColDef1 = {
+    };
+    
+    this.defaultColDef2 = {
       width: 185,
       editable: true
     };
+
+    this.rowData2 = [
+      {
+        address: 1,
+        country: 1,
+        state:1,
+        city: {id: 1, name:'NY'},
+        country1: {id:1, name: 'Indian'},
+        state1:{id :1,name:'Andhra Pradesh'}
+      },
+      {
+        address: 2,
+        country: 1,
+        state:2,
+        city: {id: 5, name:'LA'},
+        country1: {id:1, name: 'Indian'},
+        state1:{id:2, name: 'Madhya Pradesh'}
+      },
+      {
+        address: 3,
+        country: 2,
+        state:3,
+        city: {id: 9, name:'SC'},
+        country1: {id:2, name: 'USA'},
+        state1: {id:3, name: 'San Francisco'}
+      },
+      {
+        address: 4,
+        country: 3,
+        state:5,
+        city: {id: 15, name:'PA'},
+        country1: {id:3, name: 'Australian'},
+        state1: {id:5, name: 'New South Wales'},
+      }
+    ];
+
+    this.columnDefs2 = [
+      {
+        headerName: "Address",
+        field: "address",
+    
+      },
+      {
+        headerName: "Country",
+        field: "country",
+        cellEditor: "select",
+        cellEditorParams:  {
+          values: extractValues2(getCountries()),
+          cellHeight: 50,
+        },
+        valueFormatter: function(params) {
+          return lookupValue2(getCountries(), params.value);
+        },
+        valueParser: function(params) {
+          return lookupKey2(getCountries(), params.newValue);
+        },
+        onCellValueChanged: function (params: any) {
+          /**
+           * because 'select' does not offer us the possibility to use 'key-value' as traditional,
+           * we will use only values in 'select' and changed to 'id' when will be saved.
+           */
+           params.data.state=3;;
+           var rowNode = this.gridApi.getRowNode(params.data.address);
+           rowNode.setDataValue("state", 0);
+        },
+      },
+      {
+        headerName: "State",
+        field: "state",
+        cellEditor: "select",
+        cellEditorParams: function(params) {
+          var selectedCountry = params.data.country;
+          var states=getStates();
+          var value=extractValues2(states.filter(x=>{return x.country_id==selectedCountry}));
+
+          return {values:value};
+        },
+        valueFormatter: function(params) {
+          return lookupValue2(getStates(), params.value);
+        },
+        valueParser: function(params) {
+          return lookupKey2(getStates(), params.newValue);
+        },
+      },
+      {
+        headerName: "city",
+        field: "city",
+        width:100,
+        cellRenderer: (params) => params.data.city.name,
+        editable: true,
+        cellEditor: 'agRichSelectCellEditor',
+        cellEditorParams: {
+          cellHeight: 50,
+            values: getCity(),
+            cellRenderer: (params) => params.value.name
+        }
+      },
+      {
+        headerName: "country 1",
+        field: "country1",
+        width:100,
+        cellRenderer: (params) => params.data.country1.name,
+        editable: true,
+        cellEditor: 'agRichSelectCellEditor',
+        cellEditorParams: {
+          cellHeight: 50,
+            values: getCountries(),
+            cellRenderer: (params) => params.value.name
+        },
+        onCellValueChanged: function (params: any) {
+          var selectedCountry = params.data.country1.id;
+          var selectedCity = params.data.state1.id;
+          var allowedStates = getStates().filter(x=>{return x.country_id==selectedCountry});
+          var cityMismatch = allowedStates.indexOf(selectedCity) < 0;
+          if (cityMismatch) {
+            params.node.setDataValue("state1", {id:-1,name:'-select-'});
+          }
+        }
+      },
+      {
+        headerName: "state1",
+        field: "state1",
+        width:100,
+        cellRenderer: (params) => params.data.state1.name,
+        editable: true,
+        cellEditor: 'agRichSelectCellEditor',
+        cellEditorParams: function(params) {
+        var allowedStates=getStates().filter(x=>{return x.country_id==params.data.country1.id});
+        return{
+          cellHeight: 50,
+            values: allowedStates,
+            formatValue:  function(value) {
+              return value.name;
+            }
+         }
+        }
+      }
+    ]
 
    }
 
@@ -251,7 +235,7 @@ export class PayrollCenterComponent implements OnInit {
   
   bottomData = [
     {
-      payrollID: 'Total',
+      employeeAliasID: 'Total',
       payDate: '15 - 61',
       payFrequency: 'Ireland',
       payNumber: '2020',
@@ -261,11 +245,7 @@ export class PayrollCenterComponent implements OnInit {
 
     }
 ];
-// rowData = [
-//     {make: 'Toyota', model: 'Celica', price: 35000},
-//     {make: 'Ford', model: 'Mondeo', price: 32000},
-//     {make: 'Porsche', model: 'Boxter', price: 72000}
-// ];
+
 onCellValueChanged(params: any) {
 //   this.athleteService.save(params.data)
 //                      .subscribe(
@@ -291,9 +271,20 @@ onGridReady(params) {
     // this.isLoginError = true;
   });
 
- 
+  setTimeout(function() {
+    var rowCount = 0;
+    params.api.forEachNode(function(node) {
+      node.setExpanded(rowCount++ === 1);
+    });
+  }, 500);
+}
 
-  }
+onGridReady1(params) {
+  this.gridApi1 = params.api;
+  this.gridColumnApi = params.columnApi;
+}
+
+
 
   onFirstDataRendered(params) {
     params.api.sizeColumnsToFit();
@@ -306,8 +297,9 @@ onGridReady(params) {
 
   createClumns(data){
     this.columnDefs = [
-      {headerName: 'Payroll Id', field: 'payrollID', suppressSizeToFit: true},
-      {headerName: 'Payroll Date', field: 'payDate'},
+      {headerName: 'Payroll Id', field: 'employeeAliasID',
+        cellRenderer: "agGroupCellRenderer" },
+      {headerName: 'Payroll Date', field: 'payDate',editable: true,cellEditor: "datePicker"},
       {headerName: 'Deduction ID', field: 'deductions'},
       {headerName: 'Pay Frequency', field: 'payFrequency'},
       {headerName: 'Pay Number' , field: 'payNumber'},
@@ -368,49 +360,141 @@ function lookupKey(array, name) {
     if (array[i].name === name) {
         return array[i].aliasID;
     }
-  //const result = data.filter(s => s.name.includes(name))[0];
-  //return result;
-  // for (var key in mappings) {
-  //   if (mappings.hasOwnProperty(key)) {
-  //     if (name === mappings[key]) {
-  //       return key;
-  //     }
-  //   }
-  // }
+
 }
 }
 
 
-function extractValues1(mappings) {
-  return Object.keys(mappings);
+// function extractValues1(mappings) {
+//   return Object.keys(mappings);
+// }
+// function lookupValue1(mappings, key) {
+//   return mappings[key];
+// }
+// function lookupKey1(mappings, name) {
+//   for (var key in mappings) {
+//     if (mappings.hasOwnProperty(key)) {
+//       if (name === mappings[key]) {
+//         return key;
+//       }
+//     }
+//   }
+// }
+// function colorCellRenderer(params) {
+//   return "<span style='color:" + removeSpaces(params.valueFormatted) + "'>" + params.valueFormatted + "</span>";
+// }
+// function currencyFormatter(params) {
+//   var value = Math.floor(params.value);
+//   if (isNaN(value)) return "";
+//   return "\xA3" + value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+// }
+// function numberValueSetter(params) {
+//   if (isNaN(parseFloat(params.newValue)) || !isFinite(params.newValue)) {
+//     return false;
+//   }
+//   params.data.price = params.newValue;
+//   return true;
+// }
+// function removeSpaces(str) {
+//   return str ? str.replace(/\s/g, "") : str;
+// }
+
+function getCountries() {
+  return [
+    {id:0, name: '-select-'},
+    { id: 1, name: 'India' },
+    { id: 2, name: 'USA' },
+    { id: 3, name: 'Australia' }
+  ];
 }
-function lookupValue1(mappings, key) {
-  return mappings[key];
+
+function getStates() {
+  return [
+    {id:0, name: '-select-'},
+    { id: 1, country_id: 1, name: 'Andhra Pradesh' },
+    { id: 2, country_id: 1, name: 'Madhya Pradesh' },
+    { id: 3, country_id: 2, name: 'San Francisco' },
+    { id: 4, country_id: 2, name: 'Los Angeles' },
+    { id: 5, country_id: 3, name: 'New South Wales' },
+    { id: 6, country_id: 3, name: 'Victoria' },
+  ]
 }
-function lookupKey1(mappings, name) {
-  for (var key in mappings) {
-    if (mappings.hasOwnProperty(key)) {
-      if (name === mappings[key]) {
-        return key;
-      }
+
+function getCity() {
+  return [
+    {id:0, name: '-select-'},
+    { id: 1, state_id: 1, name: 'Guntur' },
+    { id: 2, state_id: 1, name: 'Vijayawada' },
+    { id: 3, state_id: 1, name: 'Nellore' },
+    { id: 4, state_id: 1, name: 'Kadapa' },
+    { id: 5, state_id: 2, name: 'Warangal' },
+    { id: 6, state_id: 2, name: 'Hyderabad' },
+    { id: 7, state_id: 2, name: 'Karimnagar' },
+    { id: 8, state_id: 2, name: 'Kadapa' },
+    { id: 9, state_id: 3, name: 'SOMA' },
+    { id: 10, state_id: 3, name: 'Richmond' },
+    { id: 11, state_id: 3, name: 'Sunset' },
+    { id: 12, state_id: 4, name: 'Burbank' },
+    { id: 13, state_id: 4, name: 'Hollywood' },
+    { id: 14, state_id: 5, name: 'Sunset' },
+    { id: 15, state_id: 5, name: 'Burbank' },
+    { id: 16, state_id: 5, name: 'Hollywood' },
+    { id: 17, state_id: 6, name: 'Benalla' },
+    { id: 18, state_id: 6, name: 'Melbourne' },
+  ]
+}
+
+function extractValues2(data) {
+  //return Object.keys(data);
+  var dep=[];
+  for (let d of data) {
+    dep.push(d.id);
+  }
+  return dep;
+};
+  //return Object.keys(mappings);
+//}
+ function lookupValue2(array, key) {
+   var value='';
+   for (var i = 0; i < array.length; i++) {
+    if (key && key==array[i].id) {
+        return array[i].name;
     }
   }
-}
-function colorCellRenderer(params) {
-  return "<span style='color:" + removeSpaces(params.valueFormatted) + "'>" + params.valueFormatted + "</span>";
-}
-function currencyFormatter(params) {
-  var value = Math.floor(params.value);
-  if (isNaN(value)) return "";
-  return "\xA3" + value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-}
-function numberValueSetter(params) {
-  if (isNaN(parseFloat(params.newValue)) || !isFinite(params.newValue)) {
-    return false;
+   //const result = data.filter(s => s.aliasID.includes(key))[0];
+   //return result;
+  //return mappings[key];
+};
+function lookupKey2(array, name) {
+  var value='';
+   for (var i = 0; i < array.length; i++) {
+    if (array[i].name === name) {
+        return array[i].id;
+    }
+
   }
-  params.data.price = params.newValue;
-  return true;
 }
-function removeSpaces(str) {
-  return str ? str.replace(/\s/g, "") : str;
+
+function getDatePicker() {
+  function Datepicker() {}
+  Datepicker.prototype.init = function(params) {
+    this.eInput = document.createElement("input");
+    this.eInput.value = params.value;
+    $(this.eInput).datepicker({ dateFormat: "dd/mm/yy" });
+  };
+  Datepicker.prototype.getGui = function() {
+    return this.eInput;
+  };
+  Datepicker.prototype.afterGuiAttached = function() {
+    this.eInput.focus();
+    this.eInput.select();
+  };
+  Datepicker.prototype.getValue = function() {
+    return this.eInput.value;
+  };
+  Datepicker.prototype.destroy = function() {};
+  Datepicker.prototype.isPopup = function() {
+    return false;
+  };
+  return Datepicker;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {LicenseManager} from "ag-grid-enterprise";
+LicenseManager.setLicenseKey("Evaluation_License_Valid_Until__24_November_2018__MTU0MzAxNzYwMDAwMA==a39c92782187aa78196ed1593ccd1527");
+
 if (environment.production) {
   enableProdMode();
 }
