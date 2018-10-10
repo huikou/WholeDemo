fe3deb8d090de53d40f52f4e33f8b8153bf1736d
Hui <huikou@yahoo.com>
2018-10-10 00:06:25 -0400

    commit

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router : Router){}
+  constructor(private router : Router,private authService: AuthService){}
+
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):  boolean {
 
-    if (localStorage.getItem('userToken') != null)
+    if (this.authService.isLoggedIn() || !this.authService.isTokenExpired())
       return true;
       this.router.navigate(['/login']);
       return false;
