a7ad17c6f946a63000092419d0a1334b4c7b88cb
Hui Kou <hui.kou@ApexHCM.com>
2018-10-12 16:36:12 -0400

    committ

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,40 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response } from "@angular/http";
-import { Observable } from 'rxjs';
-// import 'rxjs/add/operator/map';
-import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  readonly authUrl = 'https://apidev.employerondemand.com/OAuth';
+
   readonly rootUrl = 'http://localhost:50877';
+ 
   constructor(private http: HttpClient) { }
-
-  registerUser(user: User) {
-    const body: User = {
-      UserName: user.UserName,
-      Password: user.Password,
-      Email: user.Email,
-      FirstName: user.FirstName,
-      LastName: user.LastName
-    }
-    var reqHeader = new HttpHeaders({'No-Auth':'True'});
-    return this.http.post(this.authUrl + '/api/User/Register', body,{headers : reqHeader});
-  }
-
-  userAuthentication(userName, password) {
-    var data = "username=" + userName + "&password=" + password + "&grant_type=password";
-    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True',
-                        "Authorization" : "Basic QmlnWWVsbG93OlBheW1lMkFCRw==",
-                        "Accept": "application/json"});
-                        
-    return this.http.post(this.authUrl + '/token', 
-              'grant_type=client_credentials', 
-              { headers: reqHeader });
-  }
 
 
 }
