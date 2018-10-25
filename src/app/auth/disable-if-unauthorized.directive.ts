c2ba4e2126f736c487e457ca120232d83b9e4451
Hui Kou <hui.kou@ApexHCM.com>
2018-10-25 09:22:01 -0400

    commi

--- src/app/auth/disable-if-unauthorized.directive.ts
+++ src/app/auth/disable-if-unauthorized.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input} from '@angular/core';
+
+import { UserRoleService } from './user-role.service';
+import { AuthGroup } from './roleRights.types';
 
 @Directive({
   selector: '[appDisableIfUnauthorized]'
 })
 export class DisableIfUnauthorizedDirective {
 
-  constructor() { }
+  @Input('myDisableIfUnauthorized') permission: AuthGroup; // Required permission passed in
+  constructor(private el: ElementRef, private authorizationService: UserRoleService) { }
+  ngOnInit() {
+      if (!this.authorizationService.hasPermission(this.permission)) {
+            this.el.nativeElement.disabled = true;
+      }
+  }
 
 }
