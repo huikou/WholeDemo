71a821f9472acecfad1307efe230a84b687b9cd4
Hui <huikou@yahoo.com>
2018-10-25 23:38:59 -0400

    committ

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -5,13 +5,14 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';  
 import { CompanyListComponent } from '../company-list/company-list.component';
 import { ReferalSourceChartComponent } from './referal-source-chart/referal-source-chart.component';
+import { AuthGroup } from 'src/app/auth/roleRights.types';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  view:AuthGroup='VIEW_ONLY';
   constructor(private modalService: NgbModal) {     }
   showModal() {  
     this.modalService.open(CompanyListComponent,{ size: 'lg' } ).result.then(  
